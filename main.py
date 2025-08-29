62925990130609c8102ff04c075ac440b1e3e5c5
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2025-08-29 23:15:21 +0200

    main.py aktualisieren

--- main.py
+++ main.py
@@ -30,10 +30,9 @@ async def mul(x: int, y: int) -> int:
 
 @ipc_command
 async def set_title(title: str) -> bool:
-    res = await eventloop_event_register_typed("window.set_title",{"title":title},bool)
+    res = await eventloop_event_register_typed("window.set_title",{"title":title},bool) # register feutur for window settitle
     return res
-    
-
+ 
 
 
 def on_ipc(data):
@@ -48,5 +47,6 @@ if __name__ == "__main__":
     create_webframe(
         handler=on_ipc,
         html=html,
-        event_sender=gui_endless_event_loop_tasks
+        event_sender=gui_endless_event_loop_tasks, # send data to gui loop
+        handle_event_result=handle_event_loop_response # handle data result from gui loop
     )
