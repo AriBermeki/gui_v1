f88423cb4c7be450253e141f0dd5d27c946dc8c1
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2025-08-29 23:17:25 +0200

    __init__.py aktualisieren

--- frame_api/__init__.py
+++ frame_api/__init__.py
@@ -1,4 +1,11 @@
 from .command import dispatch, handle_ipc_message, ipc_command
-from .runtime_handle import handle_event_loop_response, gui_endless_event_loop_tasks
+from .runtime_handle import handle_event_loop_response, gui_endless_event_loop_tasks,eventloop_event_register_typed
 
-__all__ = ["dispatch","handle_ipc_message","ipc_command","handle_event_loop_response","gui_endless_event_loop_tasks"] 
+__all__ = [
+"dispatch",
+"handle_ipc_message",
+"ipc_command",
+"handle_event_loop_response",
+"gui_endless_event_loop_tasks", 
+"eventloop_event_register_typed"
+] 
