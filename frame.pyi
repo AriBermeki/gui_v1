47105aa3fdfc213ab3e371d4e5fa3944e6ccc49f
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2025-08-29 23:05:36 +0200

    frame.pyi aktualisieren

--- frame.pyi
+++ frame.pyi
@@ -1,4 +1,9 @@
 from typing import Callable
 
 
-def create_webframe(handler: Callable, html: str,event_sender:Callable ):...
+def create_webframe(
+    handler: Callable, 
+    html: str,
+    event_sender:Callable,
+    handle_event_result: Callable
+    ):...
